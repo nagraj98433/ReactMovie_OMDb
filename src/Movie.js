81cc8d5fa89d4b33a7acdf7c765f94fbbd194e47
nagraj98433 <rajkapool123@gmail.com>
2023-09-18 10:34:24 +0530

    click event bug fix

--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { useGlobalContext } from "./context";
 import "../src/Styles/Movie.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import MyFavorites from "./MyFavorites"; // Import the MyFavorites component
-import { useFavoriteContext } from "./FavoriteContext"; // Import the context hook
+import { useFavoriteContext } from "./FavoriteContext";
 
 const imgUrl = "https://via.placeholder.com/200/200";
 
 const Movie = () => {
   const { movie, isLoading } = useGlobalContext();
-  const { addToFavorites } = useFavoriteContext(); // Access addToFavorites from context
+  const { addToFavorites } = useFavoriteContext();
+  const [heartClicked, setHeartClicked] = useState([]);
+
+  // Initialize the clicked state based on the length of the movie array
+  useEffect(() => {
+    if (movie) {
+      setHeartClicked(Array(movie.length).fill(false));
+    }
+  }, [movie]);
+
+  const handleHeartClick = (index) => {
+    const updatedHeartClicked = [...heartClicked];
+    updatedHeartClicked[index] = !updatedHeartClicked[index];
+    setHeartClicked(updatedHeartClicked);
+  };
 
   if (isLoading) {
     return <div className="loading">Loading....</div>;
@@ -21,44 +34,45 @@ const Movie = () => {
     <>
       <section className="movie-page">
         <div className="grid grid-4-col">
-          {movie
-            ? movie.map((curMovieElem) => {
-                const { imdbID, Title, Poster } = curMovieElem;
-                const movieName = Title.substring(0, 15);
+          {movie ? (
+            movie.map((curMovieElem, index) => {
+              const { imdbID, Title, Poster } = curMovieElem;
+              const movieName = Title.substring(0, 15);
 
-                return (
-                  <div id="MovieCard" key={imdbID}>
-                    <span>
-                      <FontAwesomeIcon
-                        icon={faHeart}
-                        className="iconStyle"
-                        onClick={() => addToFavorites(curMovieElem)} // Add to favorites
-                      />
-                    </span>
-                    <NavLink to={`movie/${imdbID}`}>
-                      <div className="card">
-                        <div className="card-info">
-                          <h2>
-                            {movieName.length > 13
-                              ? `${movieName}...`
-                              : movieName}
-                          </h2>
-                          <img
-                            src={Poster === "N/A" ? imgUrl : Poster}
-                            alt="#"
-                          />
-                        </div>
+              return (
+                <div id="MovieCard" key={imdbID}>
+                  <span>
+                    <FontAwesomeIcon
+                      icon={faHeart}
+                      className={`iconStyle ${
+                        heartClicked[index] ? "red-heart" : ""
+                      }`}
+                      onClick={() => {
+                        addToFavorites(curMovieElem);
+                        handleHeartClick(index);
+                      }}
+                    />
+                  </span>
+                  <NavLink to={`movie/${imdbID}`}>
+                    <div className="card">
+                      <div className="card-info">
+                        <h2>
+                          {movieName.length > 13
+                            ? `${movieName}...`
+                            : movieName}
+                        </h2>
+                        <img src={Poster === "N/A" ? imgUrl : Poster} alt="#" />
                       </div>
-                    </NavLink>
-                  </div>
-                );
-              })
-            : ""}
+                    </div>
+                  </NavLink>
+                </div>
+              );
+            })
+          ) : (
+            <p>No movies available.</p>
+          )}
         </div>
       </section>
-
-      {/* Display the MyFavorites component */}
-      {/* <MyFavorites favorites={favorites} setFavorites={setFavorites} /> */}
     </>
   );
 };
